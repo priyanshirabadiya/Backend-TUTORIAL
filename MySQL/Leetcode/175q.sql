bfa43a728eed06ec15ae2fce5729f51e9b379dcd
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-07-07 18:13:00 +0530

    leetcodesql

--- MySQL/Leetcode/175q.sql
+++ MySQL/Leetcode/175q.sql
@@ -22,3 +22,8 @@ ON
 
 
 
+Create table If Not Exists Person2 (id int, email varchar(255));
+Truncate table Person2;
+insert into Person2 (id, email) values ('1', 'a@b.com');
+insert into Person2 (id, email) values ('2', 'c@d.com');
+insert into Person2 (id, email) values ('3', 'a@b.com');
