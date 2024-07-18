ca2fb8dd161ad22b11e4dd2e9990f2918167226a
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-07-18 19:10:42 +0530

    updated

--- MongoDB/Lecture-5/1_Aggregation.js
+++ MongoDB/Lecture-5/1_Aggregation.js
@@ -79,7 +79,7 @@ db.Mock2.aggregate([
 
 db.Mock2.find({}).limit(5).skip(3)   // give 2 records
 
-db.Mock2.aggregate([{$skip:5},{$limit:15},{$project: {id:1}}])  //give 15 records
+db.Mock2.aggregate([{ $skip: 5 }, { $limit: 15 }, { $project: { id: 1 } }])  //give 15 records
 
 db.Mock2.aggregate([
     {
@@ -161,35 +161,35 @@ db.stud.insertMany([
         "faculty": "tea001"
     },
     {
-        "studId":"std004",
-        "studName" : "sruti",
-        "course":"Ios-dev",
-        "faculty":"tea002"
+        "studId": "std004",
+        "studName": "sruti",
+        "course": "Ios-dev",
+        "faculty": "tea002"
     },
     {
-        "studId":"std005",
-        "studName" : "Raman",
-        "course":"Ui-ux dev",
-        "faculty":"tea001"
+        "studId": "std005",
+        "studName": "Raman",
+        "course": "Ui-ux dev",
+        "faculty": "tea001"
     }
 ])
 
 
 db.teach.insertMany([
     {
-        "teacherId":"tea001",
-        "teacherName":"Jignesh",
-        "hobbie":["teaching","communication"]
+        "teacherId": "tea001",
+        "teacherName": "Jignesh",
+        "hobbie": ["teaching", "communication"]
     },
     {
-        "teacherId":"tea002",
-        "teacherName":"visha",
-        "hobbie":["teach","singing"]
+        "teacherId": "tea002",
+        "teacherName": "visha",
+        "hobbie": ["teach", "singing"]
     },
     {
-        "teacherId":"tea003",
-        "teacherName":"fena",
-        "hobbie":["dance","write"]
+        "teacherId": "tea003",
+        "teacherName": "fena",
+        "hobbie": ["dance", "write"]
     }
 ])
 
@@ -197,12 +197,102 @@ db.teach.insertMany([
 
 
 
+db.stud.aggregate([
+    {
+        $lookup: {
+            from: "teach",
+            localField: "faculty",
+            foreignField: "teacherId",
+            as: "faculty"
+        }
+    },
+    {
+        $unwind: "$faculty"
+    }
+
+])
 
 
 
+// ------------------------------New universities ----------------------------------
+
+db.universities.insertMany([
+    {
+        country: 'Spain',
+        city: 'Salamanca',
+        name: 'USAL',
+        location: {
+            type: 'Point',
+            coordinates: [-5.6722512, 17, 40.9607792]
+        },
+        students: [
+            { year: 2014, number: 24774 },
+            { year: 2015, number: 23166 },
+            { year: 2016, number: 21913 },
+            { year: 2017, number: 21715 }
+        ]
+    },
+    {
+        country: 'Spain',
+        city: 'Salamanca',
+        name: 'UPSA',
+        location: {
+            type: 'Point',
+            coordinates: [-5.6691191, 17, 40.9631732]
+        },
+        students: [
+            { year: 2014, number: 4788 },
+            { year: 2015, number: 4821 },
+            { year: 2016, number: 6550 },
+            { year: 2017, number: 6125 }
+        ]
+    }
+])
+
+
+db.courses.insertMany([
+    {
+        university: 'USAL',
+        name: 'Computer Science',
+        level: 'Excellent'
+    },
+    {
+        university: 'USAL',
+        name: 'Electronics',
+        level: 'Intermediate'
+    },
+    {
+        university: 'USAL',
+        name: 'Communication',
+        level: 'Excellent'
+    }
+])
 
 
 
+// this method count all documents of project mean total count is 2
+db.universities.aggregate([
+    {
+        $project: { name: 1 }
+    },
+    {
+        $count: "count"
+    }
+])
+
+// This method first group all data as per condition then it will sum those documents 
+db.universities.aggregate([
+    {
+        $group: { _id: "$name", totaldocs: { $sum: 1 } }
+    }
+])
+
+
+db.universities.aggregate([
+    { $group : { _id : '$name', totaldocs : { $sum : 1 } } },
+    { $out : 'aggResults' }
+])
+  
 
 
 
