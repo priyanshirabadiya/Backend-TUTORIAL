da890fc5b021c5496a0ac862d33737db5e62507c
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-07-20 10:20:55 +0530

    inroduction of ts

---
+++ TypeScript/Lecture-1/intro.ts
@@ -0,0 +1,38 @@
+// tsc filename.ts => Typescript Compailer from creating js file it will create javascript file for compailation of typescript 
+// node filename.js => It will used for run a program. It will run whole program.
+
+
+
+// let a : number = 45;
+// a = 56;
+// console.log(a);
+
+
+// let b : string = "Hello world";
+// console.log(b);
+
+
+// let c : boolean = true;
+// console.log(c);
+
+
+// let big : number = 457896541;
+// console.log(typeof(big));
+// console.log(big);
+
+
+// This will not throw an error in javascript and give type of byself is number
+// let byself =  "hello" ;
+// byself = 45;
+// console.log(typeof(byself));
+
+
+const json = JSON.parse('priyanshi');
+console.log(typeof json);
+
+
+
+
+
+
+
