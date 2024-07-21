71ab27a4833c3222905c8f8d1a546b19ab439b46
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-07-21 21:58:30 +0530

    21/7/24

--- TypeScript/Lecture-2/lec2.js
+++ TypeScript/Lecture-2/lec2.js
@@ -1,4 +1,8 @@
-function boo(n) {
-    return n % 5 == 0;
+// function boo(n:number){
+//     return n % 5 == 0
+// }
+// console.log(boo(5))
+function divisable(a) {
+    return a / 32 === 0;
 }
-console.log(boo(5));
+console.log(divisable(32));
--- TypeScript/Lecture-2/lec2.ts
+++ TypeScript/Lecture-2/lec2.ts
@@ -1,5 +1,27 @@
-function boo(n:number){
-    return n % 5 == 0
-}
+// function boo(n:number){
+//     return n % 5 == 0
+// }
 
-console.log(boo(5))
+// console.log(boo(5))
+
+// function divisable(a:number):boolean{
+//     return a % 32 === 0;
+// }
+
+// console.log(divisable(32));
+
+
+
+// let maxNumber = (Number as any).MAX_SAFE_INTEGER;
+
+
+
+
+
+
+
+
+
+
+
+
