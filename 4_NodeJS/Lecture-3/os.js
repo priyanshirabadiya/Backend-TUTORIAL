42233a58b6feadb37daeea9a4ec6c571151d42af
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-08-03 22:28:48 +0530

    8/3/24

---
+++ 4_NodeJS/Lecture-3/os.js
@@ -0,0 +1,54 @@
+// const os = require('os');
+
+// console.log(os.arch()); // x64 
+// console.log(os.hostname());  // Priyanshi
+// console.log(os.homedir()); // C:\Users\HP 
+// console.log(os.networkInterfaces());
+// console.log(os.tmpdir()); // C:\Users\HP\AppData\Local\Temp 
+// console.log(os.userInfo());   
+// ans // {
+//     uid: -1,
+//     gid: -1,
+//     username: 'HP',
+//     homedir: 'C:\\Users\\HP',
+//     shell: null
+//   }
+  
+
+// console.log(os.freemem());  // 8796459008
+// console.log(os.totalmem()); // 17015463936
+// console.log(os.cpus());  // Intel(R) Core(TM) i7-8650U CPU @ 1.90GHz
+
+
+// const hello = require('./odd');
+// const {mul , add} = require('./odd')
+// console.log(hello.mul(11,5));
+// console.log(add(11,5));
+
+
+const hello = require('./odd');
+// const {mul , add} = require('./odd')
+console.log(hello.mul(10,5));
+console.log(hello.add(22,1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
