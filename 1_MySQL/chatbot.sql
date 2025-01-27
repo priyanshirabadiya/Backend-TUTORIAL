7cf8b6734b24b95209ddbbd4808fee839c5a2d65
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2025-01-27 20:47:29 +0530

    added

---
+++ 1_MySQL/chatbot.sql
@@ -0,0 +1,16 @@
+-- create database chatbot;
+use chatbot;
+
+-- create TABLE  messages(
+-- 	id int auto_increment primary key,
+--     user_message text not null,
+--     bot_response text not null,
+--     created_at TIMESTAMP default CURRENT_TIMESTAMP
+-- );
+
+-- insert into messages (id, user_message , bot_response) values (1 , "hello","hello");
+
+
+-- insert into messages (user_message , bot_response) values ("how are you","i am fine");
+
+-- select * from messages;
